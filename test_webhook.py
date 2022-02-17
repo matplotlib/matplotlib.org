7dbf8a1ad4424e8ee9fa3e2c241e89e5eb70a532
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2022-02-17 02:50:57 -0500

    Add a webhook processing server

---
+++ test_webhook.py
@@ -0,0 +1,173 @@
+"""GitHub Documentation WebHook handler tests."""
+
+import functools
+from pathlib import Path
+import subprocess
+from unittest import mock
+
+from aiohttp import web
+import pytest
+
+import webhook
+from webhook import create_app, update_repo, verify_signature
+
+
+run_check = functools.partial(subprocess.run, check=True)
+
+
+async def test_signature(monkeypatch):
+    """Test that signature verification fails and works as expected."""
+    with pytest.raises(web.HTTPBadRequest,
+                       match='Secret for non-existent was not set'):
+        await verify_signature(b'unused', 'unused', 'non-existent', 'unused')
+
+    monkeypatch.setenv('WEBHOOK_REPO_SECRET', 'abcdef')
+    with pytest.raises(web.HTTPBadRequest, match='signature was invalid'):
+        await verify_signature(b'unused', 'sha256=incorrect', 'repo', 'unused')
+
+    # Signature found by passing data to `openssl dgst -sha256 -hmac $SECRET`.
+    monkeypatch.setenv('WEBHOOK_REPO_SECRET', 'abcdef')
+    await verify_signature(
+        b'{"data": "foo"}',
+        'sha256='
+        'ebf35862e15f2ac2aa1339ebefff9a88b8270fc8a33dec8f756a398036d86329',
+        'repo',
+        'unused')
+
+
+async def test_update_repo_error():
+    """Test that updating a repository fails as expected."""
+    with pytest.raises(web.HTTPServerError,
+                       match='non-existent does not exist'):
+        await update_repo(Path('non-existent'), 'unused',
+                          'matplotlib/non-existent')
+
+
+async def test_update_repo_empty(tmp_path_factory):
+    """Test that updating an empty repository works as expected."""
+    repo1 = tmp_path_factory.mktemp('repo1')
+    run_check(['git', 'init', '-b', 'main', repo1])
+    await update_repo(repo1, 'unused', 'matplotlib/repo1')
+
+
+async def test_update_repo(tmp_path_factory):
+    """Test that updating a repository works as expected."""
+    # Set up a source repository.
+    src = tmp_path_factory.mktemp('src')
+    run_check(['git', 'init', '-b', 'main', src])
+    (src / 'readme.txt').write_text('Test repo information')
+    run_check(['git', 'add', 'readme.txt'], cwd=src)
+    run_check(['git', 'commit', '-m', 'Initial commit'], cwd=src)
+
+    # Make second from the first.
+    dest = tmp_path_factory.mktemp('dest')
+    run_check(['git', 'clone', src, dest])
+
+    # Make a second commit in source repository.
+    (src / 'install.txt').write_text('There is no installation.')
+    run_check(['git', 'add', 'install.txt'], cwd=src)
+    run_check(['git', 'commit', '-m', 'Add install information'], cwd=src)
+    src_stdout = run_check(['git', 'show-ref', '--head', 'HEAD'], cwd=src,
+                           capture_output=True).stdout.splitlines()
+    src_commit = next(
+        (line for line in src_stdout if line.split()[-1] == 'HEAD'), '')
+
+    # Now this should correctly update the first repository.
+    await update_repo(dest, 'unused', 'matplotlib/dest')
+    dest_stdout = run_check(['git', 'show-ref', '--head', 'HEAD'], cwd=dest,
+                            capture_output=True).stdout.splitlines()
+    dest_commit = next(
+        (line for line in dest_stdout if line.split()[-1] == 'HEAD'), '')
+    assert dest_commit == src_commit
+
+
+async def test_github_webhook_errors(aiohttp_client, monkeypatch):
+    """Test invalid inputs to webhook."""
+    client = await aiohttp_client(create_app())
+
+    # Only /gh/<repo-name> exists.
+    resp = await client.get('/')
+    assert resp.status == 404
+    resp = await client.get('/gh')
+    assert resp.status == 404
+
+    # Not allowed if missing correct headers.
+    resp = await client.get('/gh/non-existent-repo')
+    assert resp.status == 405
+    resp = await client.post('/gh/non-existent-repo')
+    assert resp.status == 400
+    assert 'No delivery' in await resp.text()
+    resp = await client.post('/gh/non-existent-repo',
+                             headers={'X-GitHub-Delivery': 'foo'})
+    assert resp.status == 400
+    assert 'No signature' in await resp.text()
+
+    monkeypatch.setattr(webhook, 'verify_signature',
+                        mock.Mock(verify_signature, return_value=True))
+
+    # Data should be JSON.
+    resp = await client.post(
+        '/gh/non-existent-repo',
+        headers={'X-GitHub-Delivery': 'foo', 'X-Hub-Signature-256': 'unused'},
+        data='}{')
+    assert resp.status == 400
+    assert 'Invalid data input' in await resp.text()
+
+    # Some data fields are required.
+    resp = await client.post(
+        '/gh/non-existent-repo',
+        headers={'X-GitHub-Delivery': 'foo', 'X-Hub-Signature-256': 'unused'},
+        data='{}')
+    assert resp.status == 400
+    assert 'Missing required fields' in await resp.text()
+
+    resp = await client.post(
+        '/gh/non-existent-repo',
+        headers={'X-GitHub-Delivery': 'foo', 'X-Hub-Signature-256': 'unused'},
+        data='{"action": "ping", "sender": "QuLogic", "organization": "foo",'
+             ' "repository": "foo"}')
+    assert resp.status == 400
+    assert 'incorrect organization' in await resp.text()
+
+    resp = await client.post(
+        '/gh/non-existent-repo',
+        headers={'X-GitHub-Delivery': 'foo', 'X-Hub-Signature-256': 'unused'},
+        data='{"action": "ping", "sender": "QuLogic", '
+             '"organization": "matplotlib", "repository": "foo"}')
+    assert resp.status == 400
+    assert 'incorrect repository' in await resp.text()
+
+
+async def test_github_webhook_valid(aiohttp_client, monkeypatch):
+    """Test valid input to webhook."""
+    client = await aiohttp_client(create_app())
+
+    # Do no actual work, since that's tested above.
+    monkeypatch.setattr(webhook, 'verify_signature',
+                        mock.Mock(verify_signature, return_value=True))
+    monkeypatch.setenv('SITE_DIR', 'non-existent-site-dir')
+    ur_mock = mock.Mock(update_repo, return_value=None)
+    monkeypatch.setattr(webhook, 'update_repo', ur_mock)
+
+    # Ping event just returns success.
+    resp = await client.post(
+        '/gh/non-existent-repo',
+        headers={'X-GitHub-Delivery': 'foo', 'X-Hub-Signature-256': 'unused'},
+        data='{"action": "ping", "sender": "QuLogic", "hook_id": "foo",'
+             ' "zen": "Beautiful is better than ugly.",'
+             ' "organization": "matplotlib",'
+             ' "repository": "non-existent-repo"}')
+    assert resp.status == 200
+    ur_mock.assert_not_called()
+
+    # Push event should run an update.
+    resp = await client.post(
+        '/gh/non-existent-repo',
+        headers={'X-GitHub-Delivery': 'foo', 'X-Hub-Signature-256': 'unused'},
+        data='{"action": "push", "sender": "QuLogic",'
+             ' "organization": "matplotlib",'
+             ' "repository": "non-existent-repo"}')
+    assert resp.status == 200
+    ur_mock.assert_called_once_with(
+        Path('non-existent-site-dir/non-existent-repo'), 'foo',
+        'matplotlib/non-existent-repo')
---
+++ webhook.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python3
+"""
+GitHub Documentation WebHook handler.
+
+This web server handles webhooks from GitHub in order to trigger an update of
+git checkouts. The git checkouts exist in the site directory pointed to by the
+`SITE_DIR` environment variable (defaults to `sites`).
+
+The repository is required to be in the `matplotlib` organization, and will be
+found as a subdirectory of the site directory.
+
+The secret for each repository is defined in the `WEBHOOK_{repo}_SECRET`
+environment variable, where `{repo}` is the name of the repository with special
+characters replaced by underscore and converted to upper case.
+"""
+
+import asyncio
+import hmac
+import json
+import logging
+import os
+from pathlib import Path
+import sys
+
+from aiohttp import web
+
+
+log = logging.getLogger('webhook')
+logging.basicConfig(level=logging.DEBUG)
+
+
+async def update_repo(repo: Path, delivery: str, name: str):
+    """Update a git repo at the given path."""
+    if not (repo / '.git').is_dir():
+        raise web.HTTPServerError(
+            reason=f'{delivery}: Checkout for {name} does not exist')
+
+    proc = await asyncio.create_subprocess_exec('git', 'pull', cwd=repo)
+    try:
+        await asyncio.wait_for(proc.wait(), timeout=60)
+    except asyncio.TimeoutError:
+        proc.kill()
+        raise web.HTTPServerError(
+            reason=f'{delivery}: Timed out updating git checkout of {name}')
+
+
+async def verify_signature(data: bytes, signature: bytes, repo: str,
+                           delivery: str):
+    """
+    Verify data matches signature from GitHub.
+
+    https://docs.github.com/en/developers/webhooks-and-events/webhooks/securing-your-webhooks
+    """
+    envvar = repo.replace('.', '_').replace('-', '_').upper()
+    secret = os.environb.get(f'WEBHOOK_{envvar}_SECRET'.encode('utf-8'))
+    if secret is None:
+        raise web.HTTPBadRequest(
+            reason=f'{delivery}: Secret for {repo} was not set')
+    digest = hmac.new(secret, data, 'sha256').hexdigest()
+
+    if not hmac.compare_digest(signature, f'sha256={digest}'):
+        raise web.HTTPBadRequest(
+            reason=f'{delivery}: webhook signature was invalid')
+
+
+async def github_webhook(request: web.Request):
+    """
+    Handle webhooks from GitHub.
+
+    We only handle ping and push events (this is enforced by the Caddyfile).
+    """
+    # Verify some input parameters.
+    if request.content_length > 25_000_000:  # Limit from GitHub.
+        raise web.HTTPBadRequest(reason='Request too large')
+
+    # This should be guarded against by Caddy, but check anyway.
+    # https://docs.github.com/en/developers/webhooks-and-events/webhooks/webhook-events-and-payloads#delivery-headers
+    try:
+        delivery = request.headers['X-GitHub-Delivery']
+    except KeyError:
+        raise web.HTTPBadRequest(reason='No delivery given')
+    try:
+        signature = request.headers['X-Hub-Signature-256']
+    except KeyError:
+        raise web.HTTPBadRequest(reason='No signature given')
+    repo = request.match_info.get('repo')
+    log.info('%s: Received webhook for %s', delivery, repo)
+
+    data = await request.read()
+    await verify_signature(data, signature, repo, delivery)
+
+    try:
+        data = json.loads(data)
+    except json.JSONDecodeError:
+        raise web.HTTPBadRequest(reason='Invalid data input')
+
+    # https://docs.github.com/en/developers/webhooks-and-events/webhooks/webhook-events-and-payloads#webhook-payload-object-common-properties
+    try:
+        action = data['action']
+        sender = data['sender']
+        organization = data['organization']
+        repository = data['repository']
+    except KeyError:
+        raise web.HTTPBadRequest(reason='Missing required fields')
+    log.info('%s: Received %s event from %s on %s/%s',
+             delivery, action, sender, organization, repository)
+
+    if organization != 'matplotlib':
+        raise web.HTTPBadRequest(reason=f'{delivery}: incorrect organization')
+    if repository != repo:
+        raise web.HTTPBadRequest(reason=f'{delivery}: incorrect repository')
+
+    if action == 'ping':
+        log.info('%s: Ping %s: %s', delivery, data['hook_id'], data['zen'])
+        return web.Response(status=200)
+
+    if action != 'push':
+        log.info('%s: Ignoring webhook for unused action %s', delivery, action)
+        return web.Response(status=200)
+
+    checkout = Path(os.environ.get('SITE_DIR', 'sites'), repository)
+    await update_repo(checkout, delivery, f'{organization}/{repository}')
+
+    return web.Response(status=200)
+
+
+def create_app():
+    """Create the aiohttp app and setup routes."""
+    app = web.Application()
+    app.add_routes([
+        web.post('/gh/{repo}', github_webhook),
+    ])
+    return app
+
+
+if __name__ == '__main__':
+    assert Path(os.environ.get('SITE_DIR', 'sites')).is_dir()
+
+    if len(sys.argv) > 1:
+        from urllib.parse import urlparse
+        url = sys.argv[1]
+        if not url.startswith('http://'):
+            url = f'http://{url}'
+        url = urlparse(url)
+        host = url.hostname
+        port = url.port
+    else:
+        host = 'localhost'
+        port = 8080
+    app = create_app()
+    web.run_app(app, host=host, port=port)
